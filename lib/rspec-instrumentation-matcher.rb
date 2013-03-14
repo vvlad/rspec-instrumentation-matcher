7eb6902839188d1d3b54ecaded943e3f072904f6
Vlad Verestiuc <vlad.verestiuc@me.com>
2013-03-14 06:37:03 +0200

    Moved the matcher to its own class instead of using RSpec::Matchers.define

--- lib/rspec-instrumentation-matcher.rb
+++ lib/rspec-instrumentation-matcher.rb
@@ -1,86 +1,141 @@
 require "rspec-instrumentation-matcher/version"
 
-module Rspec
+module RSpec
   module Instrumentation
     module Matcher
+      def instrument(subject)
+        InstrumentMatcher.new(subject)
+      end
+      class InstrumentMatcher
+
+        def initialize(subject)
+          @subject = subject
+          @payload = nil
+          @received = 0
+          @at_least = 1
+        end
+
+        def matches?(event_proc)
+          raise_block_syntax_error if block_given?
+
+          subscription = ActiveSupport::Notifications.subscribe @subject do |name, start, finish, id, _payload|
+            @payload = _payload
+            @received += 1
+          end
+
+          event_proc.call
+
+          ActiveSupport::Notifications.unsubscribe(subscription)
+
+          times? and at_least? and at_most? and with?
+
+        end
+
+        def failure_message
+          message = "Expected #{@subject} to be instrumented"
+          message << " at least #{count @at_least}" unless at_least?
+          message << " at most #{count @at_most}" unless at_most?
+          message << " exactly #{count @times}" unless times?
+
+          if !at_least? or !at_most? or !times?
+            message << " but was instrumented #{count @received}"
+            message << " and the payload should have been" unless with?
+          else
+            message << " with" unless with?
+          end
+
+          message << " #{@payload_expectation.inspect} instead of #{@payload.inspect}" unless with?
+
+
+          message
+
+        end
+
+        def at_least(value)
+          @at_least=value
+          self
+        end
+
+        def at_most(value)
+          @at_most = value
+          self
+        end
+
+        def once
+          times(1)
+        end
+
+        def never
+          times(0)
+        end
+
+
+
+        def times(value)
+          @times = value
+          self
+        end
+
+
+        def with(expectation)
+          @with = true
+          @payload_expectation = expectation
+          self
+        end
+
+
+        private
+        def count(value)
+          if value == 1
+            "1 time"
+          else
+            "#{value} times"
+          end
+        end
+
+        def at_least?
+          return true unless @at_least
+          @received <= @at_least
+        end
+
+        def at_most?
+          return true unless @at_most
+          @received >= @at_most
+        end
+
+        def times?
+          return true unless @times
+          @received == @times
+        end
+
+        def delta_payload
+          unmatched = @payload_expectation.reject do |key,value|
+            matched_value?(value, @payload[key]) if @payload.key? key
+          end
+        end
+
+        def with?
+          !@payload.nil? and delta_payload.size == 0
+        end
+
+
+        def matched_value?(expected, actual)
+          case expected
+          when actual then true
+          when Class then actual.is_a? expected
+          when Regexp then actual =~ expected
+          else false
+          end
+        end
+
+
+      end
     end
   end
 end
 
-RSpec::Matchers.define :instrument do |notification|
-
-  match do |event_proc|
-    raise_block_syntax_error if block_given?
-
-    @payload = nil
-    @received = 0
-    subscription = ActiveSupport::Notifications.subscribe notification do |name, start, finish, id, _payload|
-      @payload = _payload
-      @received += 1
-    end
-
-    event_proc.call
-
-    ActiveSupport::Notifications.unsubscribe(subscription)
-
-    received? and matches_payload? and right_times?
-
-  end
-
-  def at_least(value)
-    @at_least=true
-    times(value)
-  end
-
-  def at_most(value)
-    @at_most = true
-    times(value)
-  end
-
-  def once
-    at_least(1).at_most(1)
-  end
-
-
-  def times(value)
-    @times = value
-    self
-  end
-
-  def received?
-    @received > 0
-  end
-
-  def right_times?
-    true unless @times
-    success = @at_least ? @received <= @times : true
-    success = @at_most ? @received >= @times : success
-    success
-  end
-
-  def with(expectation)
-    @with = true
-    @payload_expectation = expectation
-    self
-  end
-
-
-  def matches_payload?
-    unmatched = @payload_expectation.reject do |key,value|
-      matched_value?(value, @payload[key]) if @payload.key? key
-    end
-    unmatched.size == 0
-  end
-
-
-  def matched_value?(expected, actual)
-    return true if !@with or expected == actual
-    case expected
-    when Class then actual.is_a? expected
-    when Regexp then actual =~ expected
-    else
-      false
-    end
-  end
-
+module RSpec::Matchers
+  include RSpec::Instrumentation::Matcher
 end
+
+
